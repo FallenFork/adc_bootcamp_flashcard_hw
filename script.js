2f9dd27db30f26cd5a979030371ca70ac7d69596
FallenFork <kostas.paparrizos@gmail.com>
2025-03-15 16:33:36 -0700

    second commit-added functions and event listeners

--- script.js
+++ script.js
@@ -12,10 +12,71 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
+    let flashcard = flashcards[currentIndex];
+
+    // Creating the card
+    let cardContent = document.getElementById('card-content');
+
+    if (showingTerm === true) {
+        cardContent.innerText = flashcard.term;
+    } else {
+        cardContent.innerText = flashcard.definition;
+    }
+}
+
+function flipCard() {
+    if (showingTerm === true) {
+        showingTerm = false;
+    } else {
+        showingTerm = true;
+    }
+
+    displayCard();
+}
+
+function prevCard() {
+    if (currentIndex === 0) {
+        currentIndex = flashcards.length - 1;
+    } else {
+        currentIndex--;
+    }
+
+    showingTerm = true;
+
+    displayCard();
+}
+
+function nextCard() {
+    if (currentIndex === flashcards.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+
+    showingTerm = true;
+
+    displayCard();
+}
+
+function addCard(term, definition) {
+    if (term !== '' && definition !== '') {
+        flashcards.push({ term: term, definition: definition });
+        document.getElementById('new-term').value = '';
+        document.getElementById('new-definition').value = '';
+    }
 
 }
 
 // The rest of the code you will write is apart of event listeners
+document.getElementById('flashcard').addEventListener('click', () => flipCard());
+document.getElementById('prev-btn').addEventListener('click', () => prevCard());
+document.getElementById('next-btn').addEventListener('click', () => nextCard());
+document.getElementById('add-card-btn').addEventListener('click', () => addCard(
+    document.getElementById('new-term').value,
+    document.getElementById('new-definition').value
+));
+
+
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
